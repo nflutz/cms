fe72f9e37c36b676b305e7bdd62786d9705485d9
Nicholas Lutz <nflutz@gmail.com>
2015-02-13 00:02:47 -0800

    started implementing rfc 5652 with CMSVersion

--- cms_test.go
+++ cms_test.go
@@ -1,7 +1,34 @@
 package cms
 
-import "testing"
 
-func TestCMS(t *testing.T) {
-	Decode()
+import (
+	"bytes"
+	"testing"
+	"encoding/asn1"
+	"encoding/hex"
+)
+
+
+type marshalCMSTest struct {
+	in interface {}
+	out string // Hex string representing DER encoded value
+}
+
+
+var marshalCMSTests = []marshalCMSTest{
+	{CMSVersion(1), "020101"},
+}
+
+
+func TestMarshalCMS(t *testing.T) {
+	for i, test := range marshalCMSTests {
+		data, err := asn1.Marshal(test.in)
+		if err != nil {
+			t.Errorf("#%d failed: %s", i, err)
+		}
+		out, _ := hex.DecodeString(test.out)
+		if !bytes.Equal(out, data) {
+			t.Errorf("Test #%d Failed - got: %x expected: %x\n", i + 1, data, out)
+		}
+	}
 }
---
+++ rfc5652.go
@@ -0,0 +1,8 @@
+package cms
+
+import (
+	// "encoding/asn1"
+)
+
+// CMSVersion ::= INTEGER  { v0(0), v1(1), v2(2), v3(3), v4(4), v5(5) }
+type CMSVersion int
